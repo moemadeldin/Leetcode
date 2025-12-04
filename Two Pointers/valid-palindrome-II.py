73cd958d07974c211eb741e2836ae005dc2cf72e
moemadeldin <mohamedemadeldin34@gmail.com>
2025-12-04 09:39:38 +0200

    feat: 680. Valid Palindrome II Solution

---
+++ Two Pointers/valid-palindrome-II.py
@@ -0,0 +1,25 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        
+        def isPalindrome(l, r) -> bool:
+            while l < r:
+                if not s[l].isalnum():
+                    l += 1
+                    continue
+                if not s[r].isalnum():
+                    r -= 1
+                    continue
+                if s[l].lower() != s[r].lower():
+                    return False
+                l += 1
+                r -= 1
+            return True
+
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                return isPalindrome(l + 1, r) or isPalindrome(l, r - 1)
+            l += 1
+            r -= 1
+        return True
+
